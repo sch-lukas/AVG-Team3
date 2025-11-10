0f90203466a90ff840a8668a8676ecbacf548ff2
MasterSpino <tdinoboss@gmail.com>
2025-11-10 15:12:34 +0100

    https->http

--- PaymentAPI/obj/Debug/net8.0/PaymentAPI.AssemblyInfo.cs
+++ PaymentAPI/obj/Debug/net8.0/PaymentAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaymentAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fd63913f9997f424771ecf53ce33a805a6bca66")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7dce2fe368ad94b6516ea26e3d95fdf85c7e676e")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaymentAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaymentAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
